c0928b3007608eb264330c0ec562bdc6658f5c8d
TerraStake.org <emiliano.solazzi@terrastake.org>
2025-03-08 20:00:44 -0500

    Update ChainlinkDataFeeder.sol

--- contracts/oracle/ChainlinkDataFeeder.sol
+++ contracts/oracle/ChainlinkDataFeeder.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.28;
 
-import "@openzeppelin/contracts-upgradeable-5.2/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable-5.2/security/ReentrancyGuardUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable-5.2/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable-5.2/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-5.2/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 interface ITerraStakeProjects {
